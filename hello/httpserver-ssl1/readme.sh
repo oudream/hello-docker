ae46ac2ad11d8ea55e82001f740e5898de740b1c
oudream <oudream@126.com>
2019-10-24 12:12:14 +0800

    up

--- hello/httpserver-ssl1/readme.sh
+++ hello/httpserver-ssl1/readme.sh
@@ -2,6 +2,6 @@
 
 openssl req -x509 -newkey rsa:4096 -nodes -out cert.pem -keyout key.pem -days 365
 
-docker build -t httpsserver-flask1 .
+docker build -t httpserver-ssl1 .
 
-docker run -d httpsserver-flask1
+docker run -d httpserver-ssl1
