376a9a64ec4fc06af98063e6c161c2e9f31c52b3
oudream <oudream@126.com>
2019-10-24 15:56:46 +0800

    up

--- hello/x11a/readme.sh
+++ hello/x11a/readme.sh
@@ -1,7 +1,25 @@
 #!/usr/bin/env bash
 
-docker build -t python3a .
+To get up and running, build it, then:
 
-docker run -d python3a
+docker run -it --rm -p 2150:22 firefox
+
+Add this to ~/.ssh/config on the client
+
+Host abc
+     Hostname HOST_NAME_HERE
+     Port 2150
+     user root
+     ForwardX11 yes
+     ForwardX11Trusted yes
+Connect from client with:
+
+ssh -X root@abc firefox
+
+
+docker build -t x11a .
+
+docker run -it --rm -p 2150:22 firefox
+docker run -d x11a
 
 docker exec -e DISPLAY=$DISPLAY -dti 05201d90750e /bin/sh
