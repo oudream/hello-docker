68cd5805ea2f3048d8806c617ff67f6d6406fc8c
oudream <oudream@126.com>
2019-10-29 18:01:49 +0800

    up

--- hello/alpine-ssh1/readme.sh
+++ hello/alpine-ssh1/readme.sh
@@ -3,3 +3,5 @@
 docker build -t alpine-ssh1 --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)" .
 
 docker run --rm --publish=2222:22 alpine-ssh1
+
+ssh root@localhost -p 2222  # or $(docker-machine ip default)
