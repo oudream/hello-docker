9d99afae3ccf31c368d337e31ec97263d68cd2ef
oudream <oudream@126.com>
2019-10-29 19:46:23 +0800

    up

--- hello/alpine-ssh4/readme.sh
+++ hello/alpine-ssh4/readme.sh
@@ -16,3 +16,19 @@ docker build -t alpine-ssh6 .
 docker run -d -p 2296:22 alpine-ssh6
 
 ssh root@localhost -p 2296 # or $(docker-machine ip default)
+
+
+
+docker build -t alpine-ssh7 .
+
+docker run -d -p 2297:22 alpine-ssh7
+
+ssh root@localhost -p 2297 # or $(docker-machine ip default)
+
+
+
+docker build -t alpine-ssh8 .
+
+docker run -d -p 2298:22 alpine-ssh8
+
+ssh root@localhost -p 2298 # or $(docker-machine ip default)
