9f27c2df63222f8a759d16e41b75e62ad50fd4c7
oudream <oudream@126.com>
2019-10-29 12:05:47 +0800

    up

--- projects/gcl3/alpine-ssh/readme.sh
+++ projects/gcl3/alpine-ssh/readme.sh
@@ -2,7 +2,7 @@
 
 docker build -t gcl3-alpine-ssh .
 
-docker run -d -p 2202:22 gcl3-alpine-ssh
+docker run -d -v /opt/ddd -p 2202:22 gcl3-alpine-ssh
 
 ssh -AXY -p 2202 root@35.239.31.154
 ssh -AXY -p 2202 root@localhost
