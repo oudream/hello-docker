acc421df7588b612c922a897ac70b37b95a90ba7
oudream <oudream@126.com>
2019-11-04 20:16:47 +0800

    up

--- projects/gcl3/alpine-bus/rootfs/entrypoint.sh
+++ projects/gcl3/alpine-bus/rootfs/entrypoint.sh
@@ -3,7 +3,7 @@
 # do not detach (-D), log to stderr (-e), passthrough other arguments
 /usr/sbin/sshd -e "$@"
 
+nginx -c /opt/ddd/ccpp/gcl3/deploy/nginx/nginx.conf
+
 /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_svr_bus &
-/opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_sdk_tool &
-/opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_bus_viewer &
 /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_svr_fastcgi_rtdata &
--- projects/gcl3/readme.sh
+++ projects/gcl3/readme.sh
@@ -24,6 +24,11 @@ rm -r /opt/ddd/ccpp/gcl3/build/deploy/terminal
 rm -r /opt/ddd/ccpp/gcl3/build/deploy/ui
 rm -r /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_sdk
 
+### delete all binary buf [ gcl_svr_bus liblibccxx.so gcl_svr_fastcgi_rtdata ]
+arr1=$(find . -type f)
+for a in ${arr1[@]};do [[ ${a} =~ '_d/gcl_svr_bus' || ${a} =~ '_d/liblibccxx.so' || ${a} =~ '_d/gcl_svr_fastcgi_rtdata' ]] && (echo "null ${a}") || (echo "rm ${a}" `rm ${a}` ) ; done
+for a in ${arr1[@]};do [[ ${a} =~ gcl_svr_bus$ || ${a} =~ liblibccxx.so || ${a} =~ gcl_svr_fastcgi_rtdata$ ]] && (echo "null ${a}") || (echo "rm ${a}" `rm ${a}`) ; done
+
 ### copy config
 cp -r /opt/ddd/ccpp/gcl3/deploy/config /opt/ddd/ccpp/gcl3/build/deploy/
 cp -r /opt/ddd/ccpp/gcl3/deploy/gcl_sdk /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/
@@ -41,11 +46,25 @@ cd /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d
 /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_bus_viewer &
 /opt/ddd/ccpp/gcl3/build/deploy/bin_unix_d/gcl_svr_fastcgi_rtdata &
 
-cp -r /opt/ddd/ccpp/gcl3/deploy/assets /opt/fff/gcl3.1.1/
-
 nginx -c /opt/fff/nginx/nginx.conf
 
 http://localhost:8821/bus/
+
+
+rm /opt/ddd/ccpp/gcl3/build/deploy
+
+#gcl_bus_viewer                 gcl_svr_fastcgi_filesystem     libgcl_lua_vdi.so              vxd_frame
+#gcl_sdk                        gcl_svr_fastcgi_rtdata         libgcl_sdk.so                  vxd_iec104
+#gcl_sdk_main                   gcl_svr_fastcgi_rtlog          libgcl_vdi.so                  vxd_modbus_rtu
+#gcl_sdk_tool                   gcl_svr_rtdbs                  liblibccxx.so                  vxd_modbus_tcp
+#gcl_svr_bus                    gcl_svr_script_vxd             liblibccxx_database_odbc.so
+#gcl_svr_fastcgi_database       libgcl_lua_ccxx.so             liblibccxx_database_sqlite.so
+
+
+arr1=$(find . -type f)
+for a in ${arr1[@]};do [[ ${a} =~ '_d/gcl_svr_bus' || ${a} =~ '_d/liblibccxx.so' || ${a} =~ '_d/gcl_svr_fastcgi_rtdata' ]] && (echo "null ${a}") || (echo "rm ${a}" `rm ${a}` ) ; done
+for a in ${arr1[@]};do [[ ${a} =~ gcl_svr_bus$ || ${a} =~ liblibccxx.so || ${a} =~ gcl_svr_fastcgi_rtdata$ ]] && (echo "null ${a}") || (echo "rm ${a}" `rm ${a}`) ; done
+
 
 # clion
 # error,msg="Error while executing Python code.
@@ -61,3 +80,4 @@ scp -P 2201 -r /Applications/CLion.app/Contents/bin/gdb/renderers root@localhost
 #
 echo "alias python=python3" >> ~/.bashrc
 
+limi.135246
