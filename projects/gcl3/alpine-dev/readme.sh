0cabb85e991148865ef52076c23d1558453f3ee5
oudream <oudream@126.com>
2019-11-15 20:25:04 +0800

    ssl-dev ncurses-dev python3-dev autoconf

--- projects/gcl3/alpine-dev/readme.sh
+++ projects/gcl3/alpine-dev/readme.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-cat ./../../../assets/ssh/identity.pub > ./identity.pub
 
+# build dockerfile
+cat ./../../../assets/ssh/identity.pub > ./identity.pub
 docker build -t gcl3-dev-alpine .
 
-docker run -v /opt/ddd:/opt/ddd -d -p 2231:22 gcl3-dev-alpine
 
-ssh root@localhost -p 2231 # or $(docker-machine ip default)
-
+# run on vps
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd gcl3-dev-alpine
+# on vps
+ssh -AXY root@35.232.43.174
+# on macos
 ssh root@35.232.43.174 -p 2231 -AXY -v # or $(docker-machine ip default)
 
-ssh -AXY root@35.232.43.174
+
+# run on macos(localhost)
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 gcl3-dev-alpine
+ssh root@localhost -p 2231 -AXY -v
