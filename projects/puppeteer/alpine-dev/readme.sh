ddfbda3abbf1acf9f294b11f16f5947c91801c72
oudream <oudream@126.com>
2019-11-18 19:32:05 +0800

    npm i puppeteer # Error: Failed to launch chrome!
    npm i puppeteer

--- projects/puppeteer/alpine-dev/readme.sh
+++ projects/puppeteer/alpine-dev/readme.sh
@@ -27,3 +27,19 @@ docker login
 docker tag puppeteer-dev-alpine oudream/puppeteer-dev-alpine:1.2
 docker push oudream/puppeteer-dev-alpine:1.2
 docker pull oudream/puppeteer-dev-alpine
+
+
+
+# error
+# npm i puppeteer # error at launch
+# (node:37) UnhandledPromiseRejectionWarning: Error: Failed to launch chrome! spawn /node_modules/puppeteer/.local-chromium/linux-706915/chrome-linux/chrome ENOENT
+# npm i puppeteer # Unfortunately this is not enough.
+# You will require the following Dependencies
+# sudo apt-get install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2
+# libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0
+# libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1
+# libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2
+# libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3
+# lsb-release xdg-utils wget
+### https://stackoverflow.com/questions/52993002/headless-chrome-node-api-and-puppeteer-installation
+
--- projects/puppeteer/alpine-dev2/rootfs/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/ash
-
-#
-node /opt/hello-world.js
-
-# do not detach (-D), log to stderr (-e), passthrough other arguments
-exec /usr/sbin/sshd -D -e "$@"
