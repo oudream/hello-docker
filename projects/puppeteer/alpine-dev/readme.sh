8e5c721598f58897aa50a78ae12d3c1a3fba4865
oudream <oudream@126.com>
2019-11-18 16:03:29 +0800

    up

--- projects/puppeteer/alpine-dev/readme.sh
+++ projects/puppeteer/alpine-dev/readme.sh
@@ -3,17 +3,17 @@
 
 # build dockerfile
 cat ./../../../assets/ssh/identity.pub > ./identity.pub
-docker build -t puppeteer-dev-alpine .
+docker build -t oudream/puppeteer-dev-alpine:1.1 .
 
 
 # run on vps
-docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd puppeteer-dev-alpine
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/puppeteer-dev-alpine:1.1
 # on vps
-ssh -AXY root@35.232.43.174
+ssh -AXY root@34.69.62.252
 # on macos
-ssh root@35.232.43.174 -p 2231 -AXY -v # or $(docker-machine ip default)
+ssh root@34.69.62.252 -p 2231 -AXY -v # or $(docker-machine ip default)
 
 
 # run on macos(localhost)
-docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 puppeteer-dev-alpine
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 oudream/puppeteer-dev-alpine:1.1
 ssh root@localhost -p 2231 -AXY -v
