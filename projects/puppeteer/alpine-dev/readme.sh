6850ca0b32f7ca64a5ab5ab3612fe6b0285afc12
oudream <oudream@126.com>
2019-11-18 19:07:40 +0800

    up

--- projects/puppeteer/alpine-dev/readme.sh
+++ projects/puppeteer/alpine-dev/readme.sh
@@ -6,11 +6,11 @@
 
 # build dockerfile
 cat ./../../../assets/ssh/identity.pub > ./identity.pub
-docker build -t oudream/puppeteer-dev-alpine:1.1 .
+docker build -t oudream/puppeteer-dev-alpine:1.2 .
 
 
 # run on vps
-docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/puppeteer-dev-alpine:1.1
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/puppeteer-dev-alpine:1.2
 # on vps
 ssh -AXY root@34.69.62.252
 # on macos
@@ -18,12 +18,12 @@ ssh root@34.69.62.252 -p 2231 -AXY -v # or $(docker-machine ip default)
 
 
 # run on macos(localhost)
-docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 oudream/puppeteer-dev-alpine:1.1
+docker run -d -p 2231:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 oudream/puppeteer-dev-alpine:1.2
 ssh root@localhost -p 2231 -AXY -v
 
 
 ### docker push image
 docker login
-docker tag puppeteer-dev-alpine oudream/puppeteer-dev-alpine:1.1
-docker push oudream/puppeteer-dev-alpine:1.1
+docker tag puppeteer-dev-alpine oudream/puppeteer-dev-alpine:1.2
+docker push oudream/puppeteer-dev-alpine:1.2
 docker pull oudream/puppeteer-dev-alpine
