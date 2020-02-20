1891efcaf4f7fdf4bea58095ac18c24a6ef48e19
oudream <oudream@126.com>
2020-02-21 07:26:32 +0800

    up

--- hello/ionic/ionic3-components/readme.sh
+++ hello/ionic/ionic3-components/readme.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#docker build --no-cache -t ionic3-android-builder .
-docker build -t oudream/ionic3-android-ubuntu:1.0.1 .
+#docker build --no-cache -t ionic3-components-builder .
+docker build -t oudream/ionic3-components-ubuntu:1.0.1 .
 
 docker login
-docker push oudream/ionic3-android-ubuntu:1.0.1
+docker push oudream/ionic3-components-ubuntu:1.0.1
 
-docker run -it oudream/ionic3-android-ubuntu:1.0.1 /bin/bash
+docker run -it oudream/ionic3-components-ubuntu:1.0.1 /bin/bash
 
 git clone https://github.com/yannbf/ionic3-components.git
 
