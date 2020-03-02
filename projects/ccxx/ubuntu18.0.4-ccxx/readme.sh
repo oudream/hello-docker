904f98d13865edc1b21c33ee1594db184839c2bd
oudream <oudream@126.com>
2020-03-02 20:24:23 +0800

    up ccxx docker

--- projects/ccxx/ubuntu18.0.4-ccxx/readme.sh
+++ projects/ccxx/ubuntu18.0.4-ccxx/readme.sh
@@ -2,14 +2,14 @@
 
 
 # build dockerfile
-docker build -t oudream/gcl3-bus-alpine:1.0.1 .
+docker build -t oudream/ubuntu-ccxx-env:18.04.11 .
 
 
 # run on vps
-docker run -d -p 2235:22 -v /opt/ddd:/opt/ddd gcl3-ubuntu
+docker run -d -p 2235:22 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-env:18.04.11
 ssh root@35.232.43.174 -p 2235 -AXY -v
 
 
 # run on macos(localhost)
-docker run -d -p 2235:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 5ed
+docker run -d -p 2235:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-env:18.04.11
 ssh root@localhost -p 2235 -AXY -v
--- projects/ccxx/ubuntu18.0.4-dev/readme.sh
+++ projects/ccxx/ubuntu18.0.4-dev/readme.sh
@@ -6,10 +6,10 @@ docker build -t oudream/ubuntu-ccxx-dev:18.04.11 .
 
 
 # run on vps
-docker run -d -p 2235:22 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-dev18.04.11
+docker run -d -p 2235:22 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-dev:18.04.11
 ssh root@35.232.43.174 -p 2235 -AXY -v
 
 
 # run on macos(localhost)
-docker run -d -p 2235:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 5ed
+docker run -d -p 2235:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-dev:18.04.11
 ssh root@localhost -p 2235 -AXY -v
