17a1e483a7aed5cff30abd14130426f10a21dabc
oudream <oudream@126.com>
2020-03-05 11:51:43 +0800

    up

--- projects/ccxx/ubuntu18.0.4-dev/readme.sh
+++ projects/ccxx/ubuntu18.0.4-dev/readme.sh
@@ -13,3 +13,7 @@ ssh root@35.232.43.174 -p 2205 -AXY -v
 # run on macos(localhost)
 docker run -d -p 2205:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt/ddd oudream/ubuntu-ccxx-dev:18.04.12
 ssh root@localhost -p 2205 -AXY -v
+
+
+docker run -it ubuntu:18.04 /bin/bash
+
