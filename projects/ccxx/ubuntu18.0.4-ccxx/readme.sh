9f2f5876c26b1a03e449ab6a430df4469360c81e
oudream <oudream@126.com>
2020-03-05 15:16:32 +0800

    Update readme.sh

--- projects/ccxx/ubuntu18.0.4-ccxx/readme.sh
+++ projects/ccxx/ubuntu18.0.4-ccxx/readme.sh
@@ -15,5 +15,26 @@ docker run -d -p 2235:22 -p 8821:8821 -p 8841:8841 -p 8861:8861 -v /opt/ddd:/opt
 ssh root@localhost -p 2235 -AXY -v
 
 
+
+###
+docker run -d -p 2235:22 oudream/ubuntu-ccxx-env:18.04.12
+ssh root@localhost -p 2235 -AXY -v
+
+password: 123456
+
+/opt/ccxx/build/deploy/unix/bin_d/cxtest_timer
+# or
+/opt/ccxx/build/deploy/unix/bin_d/cxtest_channel_udp_client1
+# or
+/opt/ccxx/build/deploy/unix/bin_d/cxtest_channel_udp_server1
+# or
+/opt/ccxx/build/deploy/unix/bin_d/cxsample_lua
+# or
+/opt/ccxx/build/deploy/unix/bin_d/benchmark_cxstring
+
+# ctrl + c to exit
+^c
+
+
 docker login
 docker push oudream/ubuntu-ccxx-env:18.04.12
