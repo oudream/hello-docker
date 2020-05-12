dc61bb493a1c9aa9cefd605bb565e78c6b450843
huan-rabbit <7266903@qq.com>
2020-05-12 22:21:36 +0800

    20200512-1

--- assets/pinfox/pl10_safe_lock/views/home/Home.vue
+++ assets/pinfox/pl10_safe_lock/views/home/Home.vue
@@ -196,9 +196,9 @@
 				border-color: rgba(238,241,146,0.3);
 				border-right-width: 1px;
 				border-right-style: solid;
-				background: url(../../img/app.png);
-				background-size: auto,100%;
-				background-repeat:no-repeat;
+				//background: url(../../img/app.png);
+				//background-size: auto,100%;
+				//background-repeat:no-repeat;
 				img {
 					width: 40px;
 					float: left;
